400f9bffcefc4faaffbbbeacb13a2f12ec483832
zealsham <shammahagwor@gmail.com>
2016-08-18 01:54:23 +0100

    the jslint commit for working api

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
   $('button').click(function(){
-    //load the ajax request and api in JSONP format
+    // load the ajax request and api in JSONP format
     $.ajax({
-      url:"http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
-      dataType:"json",
-      async: true,//allows other code to run .non blocking
-      success: function(data){//call back for sucessful request
+      url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+      dataType: "json",
+      async: true, // allows other code to run .non blocking
+      success: function(data){// call back for sucessful request
         $('#quote-text').html(JSON.stringify(data));
       }
 
 
-    })
+    });
   });
 });
